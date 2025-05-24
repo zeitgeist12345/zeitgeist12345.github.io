7f7f4bb005ddb892a9a235655898b035deeceb67
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-05-24 13:52:28 +0530

    Done

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 My simple website
 
 Avoid javascript as much as possible.
+Currently there is no javascript at all.
+
+There is no use of absolute px.
 
 Should work on both desktop and mobile.
--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const heading = document.querySelector('h1');
-    heading.addEventListener('click', function () {
-        alert('Hello from JavaScript!');
-    });
-});
